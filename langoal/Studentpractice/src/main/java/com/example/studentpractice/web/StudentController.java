80ccdb36d32e26cbd4b1e44aa9863529fc61d5af
lina2chi <lina2chi@gmail.com>
2022-11-01 12:08:09 -0700

    changes from dev

---
+++ langoal/Studentpractice/src/main/java/com/example/studentpractice/web/StudentController.java
@@ -0,0 +1,34 @@
+package com.example.studentpractice.web;
+
+import com.example.studentpractice.entities.Student;
+import com.example.studentpractice.repositories.StudentRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class StudentController {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @GetMapping(path = "/index")
+    public String students(Model model, @RequestParam(name = "keyword", defaultValue = "")
+    String keyword) {
+//
+        List<Student> students;
+        if (keyword.isEmpty()) {
+            students = studentRepository.findAll();
+        } else {
+            long key = Long.parseLong(keyword);
+            students = studentRepository.findStudentById(key);
+        }
+        model.addAttribute("listStudents", students);
+        return "students";
+    }
+}
